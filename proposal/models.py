964ea9b33596c33ff789bf78ffa3d082b7744436
Francisco Luque <fluque1995@gmail.com>
2020-09-07 16:24:38 +0200

    Removed trials now unused

--- proposal/models.py
+++ proposal/models.py
@@ -1,25 +1,5 @@
 import keras
 
-def cnnlstm_model():
-    input_layer = keras.layers.Input(shape=(16,299,299,3))
-
-    feature_extractor = keras.layers.TimeDistributed(keras.applications.Xception(
-        include_top=False, weights='imagenet'), trainable=False)(input_layer)
-
-    recurrent_layer = keras.layers.ConvLSTM2D(128, kernel_size=3)(feature_extractor)
-
-    batch_norm = keras.layers.BatchNormalization()(recurrent_layer)
-    dropout = keras.layers.Dropout(0.6)(batch_norm)
-    flatten = keras.layers.Flatten()(dropout)
-    linear = keras.layers.Dense(128)(flatten)
-    linear = keras.layers.Dropout(0.6)(linear)
-    relu = keras.layers.Activation('relu')(linear)
-    predictions = keras.layers.Dense(101, activation='softmax')(relu)
-
-    model = keras.models.Model(inputs=input_layer, outputs=predictions)
-    return model
-
-
 def recurrent_feats_model():
 
     xception = keras.applications.Xception(include_top=True, weights='imagenet')
@@ -40,20 +20,6 @@ def recurrent_feats_model():
     linear = keras.layers.Dense(512, activation='relu')(recurrent_layer)
     linear = keras.layers.Dropout(0.5)(linear)
     linear = keras.layers.Dense(128, activation='relu')(linear)
-    linear = keras.layers.Dropout(0.5)(linear)
-    predictions = keras.layers.Dense(101, activation='softmax')(linear)
-
-    model = keras.models.Model(inputs=input_layer, outputs=predictions)
-    return model
-
-def recurrent_backbone():
-    input_layer = keras.layers.Input((25,2048))
-    recurrent_layer = keras.layers.LSTM(
-        512,
-        return_sequences=False,
-        dropout=0.5
-    )(input_layer)
-    linear = keras.layers.Dense(256, activation='relu')(recurrent_layer)
     linear = keras.layers.Dropout(0.5)(linear)
     predictions = keras.layers.Dense(101, activation='softmax')(linear)
 
