56bad613a33675b0083b11c285837b4bcfb79628
Francisco Luque <fluque1995@gmail.com>
2020-09-10 17:58:36 +0200

    White space

--- proposal/classifier.py
+++ proposal/classifier.py
@@ -7,6 +7,7 @@ from keras.regularizers import l2
 import configuration as cfg
 
 def classifier_model():
+
     model = Sequential()
     model.add(Dense(512, input_dim=1024, kernel_initializer='glorot_normal', kernel_regularizer=l2(0.001), activation='relu'))
     model.add(Dropout(0.5))
